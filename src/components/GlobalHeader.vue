bebeb3befc82b77a59f964f9f38dbaa5cf861a46
cq2021-coder <2972084238@qq.com>
2023-07-28 00:45:41 +0800

    add: 搭建基础结构

---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,73 @@
+<template>
+  <a-row id="globalHeader" style="margin-bottom: 16px" align="center">
+    <a-col flex="auto">
+      <a-menu
+        mode="horizontal"
+        :selected-keys="selectedKeys"
+        @menu-item-click="doMenuClick"
+      >
+        <a-menu-item
+          key="0"
+          :style="{ padding: 0, marginRight: '38px' }"
+          disabled
+        >
+          <div class="title-bar">
+            <img class="logo" src="../assets/logo.png" />
+            <div class="title">程崎 OJ</div>
+          </div>
+        </a-menu-item>
+        <a-menu-item v-for="item in routes" :key="item.path">
+          {{ item.name }}
+        </a-menu-item>
+      </a-menu>
+    </a-col>
+    <a-col flex="100px">
+      <div>
+        {{ store.state.user?.loginUser?.userName ?? "未登录" }}
+      </div>
+    </a-col>
+  </a-row>
+</template>
+
+<script lang="ts" setup>
+import { routes } from "@/router/routes";
+import { useRouter } from "vue-router";
+import { ref } from "vue";
+import { useStore } from "vuex";
+
+const router = useRouter();
+const store = useStore();
+const selectedKeys = ref(["/"]);
+router.afterEach((to, from, failure) => {
+  selectedKeys.value = [to.path];
+  console.log(from, failure);
+});
+const doMenuClick = (key: string) => {
+  router.push({
+    path: key,
+  });
+};
+
+// setTimeout(() => {
+//   store.dispatch("getLoginUser", {
+//     userName: "程崎",
+//     role: "admin",
+//   });
+// }, 3000);
+</script>
+
+<style scoped>
+.title-bar {
+  display: flex;
+  align-items: center;
+}
+
+.title {
+  color: #444;
+  margin-right: 16px;
+}
+
+.logo {
+  height: 48px;
+}
+</style>
---
+++ src/store/user.ts
@@ -0,0 +1,23 @@
+/* eslint-disable */
+import { StoreOptions } from "vuex";
+
+export default {
+  namespace: true,
+  state: () => ({
+    loginUser: {
+      userName: "未登录",
+      role: "notLogin",
+    },
+  }),
+  actions: {
+    getLoginUser({ commit, state }, payload) {
+      console.log(state);
+      commit("updateUser", payload);
+    },
+  },
+  mutations: {
+    updateUser(state, payload) {
+      state.loginUser = payload;
+    },
+  },
+} as StoreOptions<any>;
