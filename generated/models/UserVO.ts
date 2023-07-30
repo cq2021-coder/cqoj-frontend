76dbf67f9f0eaee0beedd17def99a8ee2ed63932
cq2021-coder <2972084238@qq.com>
2023-07-31 00:15:26 +0800

    add: 前后端联调

---
+++ generated/models/UserVO.ts
@@ -0,0 +1,24 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type UserVO = {
+    createTime?: string;
+    id?: number;
+    userAvatar?: string;
+    userName?: string;
+    userProfile?: string;
+    userRole?: UserVO.userRole;
+};
+
+export namespace UserVO {
+
+    export enum userRole {
+        ADMIN = 'ADMIN',
+        BAN = 'BAN',
+        USER = 'USER',
+    }
+
+
+}
